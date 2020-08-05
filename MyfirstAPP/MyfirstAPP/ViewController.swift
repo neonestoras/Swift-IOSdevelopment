ee9135f2a5f4dca836bb1633869cdd582806493d
Nestoras Neofytou <47972485+neonestoras@users.noreply.github.com>
2020-08-05 16:36:59 +0300

    xcode project created
    xcode project created

---
+++ MyfirstAPP/MyfirstAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyfirstAPP
+//
+//  Created by nestoras neofytou on 05/08/2020.
+//  Copyright © 2020 SmartCyties. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
